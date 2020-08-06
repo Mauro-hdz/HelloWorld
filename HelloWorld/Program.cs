10557f44ca284a21068d29646e930be33f95d197
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-08-05 20:44:39 -0500

    Add more variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,9 +8,13 @@ namespace HelloWorld
         {
 
             int count = 20;
+            double num = 2.49;
+            char character = 'L';
             string intro = "Hello user I am a C# program";
             Console.WriteLine(intro);
-            Console.WriteLine($"count is: {count}");
+            Console.WriteLine($"count is: {count}.");
+            Console.WriteLine(character + "is the first letter in loser.");
+            Console.WriteLine($"The price of a bag of chips is {num}.");
         }
     }
 }
